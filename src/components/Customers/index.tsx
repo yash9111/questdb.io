defcd5103a8f2adf03f2fde4bdc5e4821ed07aa1
Maciej Bodek <maciej.bodek@gmail.com>
2023-01-13 10:49:20 +0100

    defer loading marquee

--- src/components/Customers/index.tsx
+++ src/components/Customers/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useLayoutEffect, useState } from "react"
 import { Section } from "../Section"
 import styles from "./styles.module.css"
 import Marquee from "react-fast-marquee"
@@ -7,13 +7,21 @@ import { Logos } from "./logos"
 
 const Customers = () => {
   const { isDarkTheme } = useThemeContext()
+  const [rendered, setRendered] = useState(false)
+
+  useLayoutEffect(() => {
+    setRendered(true)
+  }, [])
+
   return (
     <Section noGap className={styles.section}>
-      <Marquee gradientColor={isDarkTheme ? [33, 34, 44] : [255, 255, 255]}>
-        <div className={styles.logos}>
-          <Logos isDarkTheme={isDarkTheme} />
-        </div>
-      </Marquee>
+      {rendered && (
+        <Marquee gradientColor={isDarkTheme ? [33, 34, 44] : [255, 255, 255]}>
+          <div className={styles.logos}>
+            <Logos isDarkTheme={isDarkTheme} />
+          </div>
+        </Marquee>
+      )}
     </Section>
   )
 }
