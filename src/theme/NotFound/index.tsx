18b98c811a3fd51279aea5e9f3637b3987698217
Arijus Šukys <argshook@gmail.com>
2023-01-18 15:49:22 +0200

    chore(ui): update 404 page (#1315)

--- src/theme/NotFound/index.tsx
+++ src/theme/NotFound/index.tsx
@@ -1,18 +1,47 @@
 import React from "react"
-
 import Layout from "../Layout"
-
+import { Section } from "../../components/Section"
+import SearchBar from "@theme/SearchBar"
+import Link from "@docusaurus/Link"
 import styles from "./styles.module.css"
 
-const NotFound = () => (
-  <Layout flex title="Page not found">
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>404</h1>
-      <p className={styles.message}>
-        We could not find what you were looking for.
-      </p>
-    </div>
-  </Layout>
-)
+const NotFound = ({ location }) => {
+  const missingLink = location.pathname.replace("/", "")
+
+  return (
+    <Layout title="Page not found">
+      <Section>
+        <Section.Title size="small">
+          404 - <code className={styles.missingLink}>{missingLink}</code> not
+          found
+        </Section.Title>
+
+        <Section.Subtitle>
+          Sorry, couldn&apos;t find this page :(
+        </Section.Subtitle>
+
+        <div className={styles.search}>
+          <SearchBar />
+        </div>
+
+        <div>
+          <Section.Subtitle>Or check out other content</Section.Subtitle>
+          <ul className={styles.otherContentLinks}>
+            {[
+              { to: "/cloud/", label: "QuestDB Cloud" },
+              { to: "/blog/", label: "Read our Blog" },
+              { to: "/get-questdb/", label: "Download QuestDB" },
+              { to: "/docs/", label: "Read our Docs" },
+            ].map(({ to, label }) => (
+              <li key={to}>
+                <Link to={to}>{label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Section>
+    </Layout>
+  )
+}
 
 export default NotFound
