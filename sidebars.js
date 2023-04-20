cc969b492fdfdc63d70ab91cb831f4e2bac1f16b
Arijus Šukys <argshook@gmail.com>
2023-04-20 12:09:32 +0300

    add apache spark blog post link to 3rd party category in sidebar (#1599)
    * add apache spark blog post link to 3rd party category in sidebar
    
    * move to top to keep alphabetical order
    
    * use only `Spark` instead of `Apache Spark`

--- sidebars.js
+++ sidebars.js
@@ -123,6 +123,14 @@ module.exports = {
         "third-party-tools/pandas",
         "third-party-tools/prometheus",
         "third-party-tools/redpanda",
+        {
+          label: "Spark",
+          customProps: {
+            tag: "New",
+          },
+          type: "link",
+          href: "/blog/integrate-apache-spark-questdb-time-series-analytics",
+        },
         "third-party-tools/telegraf",
       ],
     },
@@ -270,7 +278,7 @@ module.exports = {
       items: [
         "troubleshooting/faq",
         "troubleshooting/log",
-        "troubleshooting/os-error-codes"
+        "troubleshooting/os-error-codes",
       ],
     },
     {
